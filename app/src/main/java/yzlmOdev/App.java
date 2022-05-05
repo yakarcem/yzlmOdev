549919292d542c238e819a44b1474748a4be894e
yakarcem <yakarcem@gmail.com>
2022-05-05 23:02:27 +0300

    cmmt

--- app/src/main/java/yzlmOdev/App.java
+++ app/src/main/java/yzlmOdev/App.java
@@ -60,7 +60,7 @@ if(result == true){
  if (processBuilder.environment().get("PORT") != null) {
  return Integer.parseInt(processBuilder.environment().get("PORT"));
  }
- return 2525; //return default port if heroku-port isn't set (i.e. on localhost)
+ return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
  }
 }
 
